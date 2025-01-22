0d16328b69006f7a9f5b699553d1027d8be5564e
Filip Napiorkowski <filipnapiorkowski90@wp.pl>
2025-01-22 22:54:14 +0000

    Added password visibility and login UI improvement

--- kotlin-blog/tailwind.config.js
+++ kotlin-blog/tailwind.config.js
@@ -8,9 +8,7 @@ export default {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['"Josefin Sans"', 'sans-serif'], // Set Josefin Sans as the default sans font
-      },
+      
     },
   },
   plugins: [],
